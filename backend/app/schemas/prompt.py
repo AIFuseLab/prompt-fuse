5488eb8ae1fc04b50621623faf447fd5a8b0ce4d
Tarek Swaidane <swaidanetarek.ts@gmail.com>
2024-12-02 20:20:34 +0200

    Hot fix (#9)
    * fix update prompt issue
    
    * fix update prompt issue

--- backend/app/schemas/prompt.py
+++ backend/app/schemas/prompt.py
@@ -8,7 +8,7 @@ class PromptCreate(BaseModel):
     prompt: Optional[str] = None
     notes: Optional[str] = None
     version: Optional[float] = 0.0
-    llm_id: Optional[UUID] = None
+    llm_id: Optional[UUID | str] = None
     prompt_template_id: Optional[UUID] = None
 
 class PromptUpdate(BaseModel):
@@ -16,7 +16,7 @@ class PromptUpdate(BaseModel):
     prompt: Optional[str] = None
     notes: Optional[str] = None
     version: Optional[float] = None
-    llm_id: Optional[UUID] = None
+    llm_id: Optional[UUID | str] = None
     prompt_template_id: Optional[UUID] = None
 
 class PromptResponse(BaseModel):
@@ -26,7 +26,7 @@ class PromptResponse(BaseModel):
     notes: Optional[str] = None
     creation_date: datetime
     version: Optional[float] = None
-    llm_id: Optional[UUID] = None
+    llm_id: Optional[UUID | str] = None
     llm_model_name: Optional[str] = None
     prompt_template_id: Optional[UUID] = None
 
